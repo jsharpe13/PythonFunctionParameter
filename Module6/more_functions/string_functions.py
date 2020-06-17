3ee7b2d2e06a0566b3e7da86b947dc3cb4f82b2a
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-16 20:22:18 -0500

    final push

--- Module6/more_functions/string_functions.py
+++ Module6/more_functions/string_functions.py
@@ -1,5 +1,23 @@
+"""
+Program: cast_to_integer.py
+Author: Jacob Sharpe
+Last date modified: 6/16/2020
+
+The purpose of this program is to take in a string and an number and output
+the string message the amount given
+"""
+
+""" this takes the message string and the number of times to output and returns the string
+:param message string, number of times to output
+:returns the created string from the for loop
+"""
 def multiply_string(message, n):
-    pass
+    results = ""
+    for x in range(n):
+        results = results + message
+    return results
+
 
 if __name__ == '__main__':
-    pass
+    result = multiply_string("Jacob", 3)
+    print(result)
